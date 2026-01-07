7ebb00446fcdd13bfd536c6d6de8e43d28353c2a
Donny <tanadornrach@gmail.com>
2026-01-07 21:31:52 +0700

    replace hard-coded mongodb uri to be an environment variable

--- backend/database.js
+++ backend/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const Thing = require("./model/thing_model.js");
 const User = require("./model/user_model.js");
@@ -8,7 +9,7 @@ main().catch(err => console.error(err));
 //this function is used for managing in MongoDB database
 async function main() {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/soft-power");
+        await mongoose.connect(process.env.MONGODB_URI);
 
         mongoose.connection.on("error", err => {
             console.error(err);
